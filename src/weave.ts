4f90df6da7a099d9bf0d7667a2cdb9a6cebc0f75
Joni Katajamäki <joni.katajamaki@icloud.com>
2024-11-15 07:59:42 +0200

    Fix class providers as dependency of function providers

--- src/weave.ts
+++ src/weave.ts
@@ -1,14 +1,19 @@
-import type { Provider, ProviderParam, Providers, Resolved } from "./Providers";
+import type {
+  ClassProvider,
+  Provider,
+  ProviderParam,
+  Providers,
+  Resolved,
+} from "./Providers";
 import { resolved } from "./resolved";
 import { verifyDependencies } from "./verifyDependencies";
 
-export type AllProviderDependencies<
-  D extends Record<string, Provider>
-> = UnionToIntersection<
-  {
-    [P in keyof D]: ProviderDependencies<D[P]>;
-  }[keyof D]
->;
+export type AllProviderDependencies<D extends Record<string, Provider>> =
+  UnionToIntersection<
+    {
+      [P in keyof D]: ProviderDependencies<D[P]>;
+    }[keyof D]
+  >;
 
 // prettier-ignore
 export type ProviderDependencies<F> =
@@ -24,12 +29,12 @@ type UnionToIntersection<Union> = (
   ? Intersection
   : never;
 
+// prettier-ignore
 export const weave = <
   P extends _InternalProviders & {
     // Inlined FullyResolved type for more readable error messages
-    [K in keyof AllProviderDependencies<_InternalProviders>]: (
-      ...args: ProviderParams<_InternalProviders, K>
-    ) => AllProviderDependencies<_InternalProviders>[K];
+    [K in keyof AllProviderDependencies<_InternalProviders>]:
+    FullyResolvedProvider<_InternalProviders, K>;
   },
   _InternalProviders extends Providers = P
 >(
@@ -44,6 +49,15 @@ export type FullyResolved<D extends Providers> = {
     arg: any
   ) => AllProviderDependencies<D>[K];
 };
+
+export type FullyResolvedProvider<
+  P extends Providers,
+  K extends keyof AllProviderDependencies<P>
+> = K extends keyof P
+  ? P[K] extends ClassProvider<any, any>
+    ? new (...arg: ProviderParams<P, K>) => AllProviderDependencies<P>[K]
+    : (...arg: ProviderParams<P, K>) => AllProviderDependencies<P>[K]
+  : never;
 
 export type ProviderParams<P extends Providers, K> = K extends keyof P
   ? [deps: ProviderParam<P[K]>]
